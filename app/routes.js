6bd43a6f9423cc97331b48f5de629d7164770b33
Joseph Cyrus <Joseph.Cyrus@valtech.com>
2019-11-25 15:48:07 +0000

    Implement nunjucks syntax and routing

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.get('/start', function (req, res) {
+  res.render('start', {'message' : 'Hello world'});
+});
 
 module.exports = router
